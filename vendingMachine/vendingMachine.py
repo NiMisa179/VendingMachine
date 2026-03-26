3afc7fb9d820e1ad9aa94a1329995f5b45c30d95
Nikos Misailidis <93082268+NiMisa179@users.noreply.github.com>
2026-03-26 23:51:41 +0200

    Update vendingMachine.py

--- vendingMachine/vendingMachine.py
+++ vendingMachine/vendingMachine.py
@@ -11,4 +11,4 @@ monoevra = (change % 200) // 100
 misoevra = ((change % 200) % 100) // 50
 dekalepta = (((change % 200) % 100) % 50) // 10
 
-print("\n", dievra, ":2\u20ac \n", monoevra, ":1\u20ac \n", misoevra, ":0,5\u20ac \n", dekalepta, ":0,1\u20ac \n")
+print("\n", dievra, ":2\u20ac \n", monoevra, ":1\u20ac \n", misoevra, ":0,5\u20ac \n", dekalepta, ":0,1\u20ac")
