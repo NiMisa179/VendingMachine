eaf1ab8e2b40cff4d29d178b7ba82e37ad79799e
NiMisa179 <nikosmisailidis26@gmail.com>
2026-03-27 20:23:58 +0200

    print edit 3

--- vendingMachine.py
+++ vendingMachine.py
@@ -14,7 +14,7 @@ for i in range(len(vendM)):
     print(f"{i+1:<3}{vendM[i]:<22}{prices[i]:<10}")
 print("----------------------------")
 
-print("IT WORKs!!!!")
+print("IT WORKs!!!")
 
 pId = int(input("Type the product 's ID: "))
 
